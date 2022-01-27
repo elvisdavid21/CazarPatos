625cd2cdf5d36fdc5008a8216b98cb5e65777e7f
Elvis Montaluisa <elvis.montaluisa@epn.edu.ec>
2022-01-27 15:52:18 -0500

    RecyclerView

---
+++ app/src/main/java/com/montaluisaelvis/cazarpatos/Jugador.kt
@@ -0,0 +1,6 @@
+package com.montaluisaelvis.cazarpatos
+
+data class Jugador (var usuario:String, var patosCazados:Int){
+    constructor():this("",0)
+}
+
---
+++ app/src/main/java/com/montaluisaelvis/cazarpatos/RankingActivity.kt
@@ -0,0 +1,27 @@
+package com.montaluisaelvis.cazarpatos
+
+//import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+
+        var jugadores = arrayListOf<Jugador>()
+        jugadores.add(Jugador("Jugador1",10))
+        jugadores.add(Jugador("Jugador2",6))
+        jugadores.add(Jugador("Jugador3",3))
+        jugadores.add(Jugador("Jugador4",9))
+        jugadores.sortByDescending { it.patosCazados }
+
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+        recyclerViewRanking.adapter = RankingAdapter(jugadores);
+        recyclerViewRanking.setHasFixedSize(true);
+
+    }
+}
